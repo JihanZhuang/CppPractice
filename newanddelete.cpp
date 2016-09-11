087441fbe516dfff3ba8ce506042bc4eb9af713c
JihanZhuang <zhuanghan1103@163.com>
2016-09-11 18:50:01 +0800

    new and delete is mainly for class,it can also insteaded of by malloc and free if you do not need to call construct or destruct for a class

---
+++ newanddelete.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: newanddelete.cpp
+    > Author: Han Zhuang
+    > Mail: zhuanghan1103@163.com 
+    > Created Time: 2016年09月11日 星期日 17时55分05秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+class test{
+	public:
+	int x;
+	test():x(10){
+
+	}
+};
+int main(){
+
+}
