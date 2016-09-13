8f2ca9eaeb7104795bce1c47b3d37cb9e5cabad6
JihanZhuang <zhuanghan1103@163.com>
2016-09-13 19:56:48 +0800

    after linux 2.6,fork will copy by write,so multi process will optimize

---
+++ fork.c
@@ -0,0 +1,32 @@
+/*************************************************************************
+    > File Name: fork.c
+    > Author: Han Zhuang
+    > Mail: zhuanghan1103@163.com 
+    > Created Time: 2016年09月13日 星期二 19时16分00秒
+ ************************************************************************/
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int pid;
+	char *p;
+	p=(char *)malloc(sizeof(char)*10);
+	p[0]='c';
+	p[1]='a';
+	p[2]='\0';
+	pid=fork();
+	if(pid==0){
+		while(1){
+			printf("i am parent,point is %p,str is %s\n",p,p);
+			sleep(1);
+		}
+	}else{
+		while(1){
+			printf("i am child,point is %p,str is %s\n",p,p);
+			p[0]='w';
+			p[1]='k';
+			sleep(1);
+		}
+	}
+	return 1;
+}
