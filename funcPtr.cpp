f7a781ec5cf87f6629beedbc5917aac7f16304e1
JihanZhuang <zhuanghan1103@163.com>
2016-09-24 09:47:11 +0800

    use function ptr to create callback function

---
+++ funcPtr.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+    > File Name: funcPtr.cpp
+    > Author: Han Zhuang
+    > Mail: zhuanghan1103@163.com 
+    > Created Time: 2016年09月24日 星期六 09时44分27秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int add(int a,int b){
+	return a+b;
+}
+
+int test(int (*pf)(int,int)){
+	int a=1,b=3;
+	return pf(a,b);
+}
+int main(){
+	int i;
+	i=test(add);
+	cout<<i<<endl;
+	return 1;
+}
